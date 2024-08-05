1411bdcc28a0b9555cabce3dcf5d8fae2c479952
maicol tabares <maicoltabares65@gmail.com>
2024-08-05 13:51:12 -0500

    commit 4

--- src/components/header.ts
+++ src/components/header.ts
@@ -7,7 +7,7 @@ export function HeaderHome() {
         <header class="header">
             <h1 class="header-title">Post</h1>
             <div class="header-buttons">
-                <button id="create-post" class="btn_header">Crear POST</button>
+                <button id="create-post" class="btn_header">Crear un post</button>
                 <button id="logout" class="btn_header">Salir</button>
             </div>
         </header>
